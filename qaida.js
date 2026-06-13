a70c488ab56123322aecb912c19317c639a994f5
almasmaniyar <123802819+almasmaniyar@users.noreply.github.com>
2026-06-13 06:00:00 +0530

    Add files via upload

---
+++ qaida.js
@@ -0,0 +1,27 @@
+// Qaida letters and Harakat options used by the Qaida Practice mode.
+
+export const HARAKAT_OPTIONS = [
+  { id: 'plain', label: 'Plain', urdu: 'No Harakat', mark: '' },
+  { id: 'fatha', label: 'Zabar', urdu: 'Fatha', mark: '\u064E' },
+  { id: 'kasra', label: 'Zer', urdu: 'Kasra', mark: '\u0650' },
+  { id: 'damma', label: 'Pesh', urdu: 'Damma', mark: '\u064F' },
+];
+
+export const QAIDA_LETTERS = [
+  { id: 'alif', name: 'Alif', char: '\u0627', freq: 392 },
+  { id: 'baa', name: 'Baa', char: '\u0628', freq: 415 },
+  { id: 'taa', name: 'Taa', char: '\u062A', freq: 440 },
+  { id: 'thaa', name: 'Thaa', char: '\u062B', freq: 466 },
+  { id: 'jeem', name: 'Jeem', char: '\u062C', freq: 494 },
+  { id: 'hhaa', name: 'Haa', char: '\u062D', freq: 523 },
+  { id: 'khaa', name: 'Khaa', char: '\u062E', freq: 554 },
+  { id: 'daal', name: 'Daal', char: '\u062F', freq: 587 },
+  { id: 'dhaal', name: 'Dhaal', char: '\u0630', freq: 622 },
+  { id: 'raa', name: 'Raa', char: '\u0631', freq: 659 },
+  { id: 'zaa', name: 'Zaa', char: '\u0632', freq: 698 },
+  { id: 'seen', name: 'Seen', char: '\u0633', freq: 740 },
+  { id: 'sheen', name: 'Sheen', char: '\u0634', freq: 784 },
+  { id: 'saad', name: 'Saad', char: '\u0635', freq: 831 },
+  { id: 'daad', name: 'Daad', char: '\u0636', freq: 880 },
+  { id: 'ayn', name: 'Ayn', char: '\u0639', freq: 932 },
+];
